a431695aa3ff9f469dfa36a6b7a2d967fc599839
caedes <romain.laurent@wopata.com>
2014-08-09 01:48:14 +0200

    Add gulp-stylus

--- tasks/build.js
+++ tasks/build.js
@@ -1,9 +1,14 @@
 var gulp  = require('gulp');
 var jade = require('gulp-jade');
+var stylus = require('gulp-stylus');
 
 var config = require('../package.json').config;
 
 gulp.task('build', ['clean'], function () {
+  gulp.src('src/main.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest(config.paths.build));
+
   gulp.src('src/index.jade')
     .pipe(jade())
     .pipe(gulp.dest(config.paths.build));
