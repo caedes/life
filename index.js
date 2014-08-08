a1afd668119d91a915527b3031a62bb59d083a24
caedes <romain.laurent@wopata.com>
2014-08-09 01:34:59 +0200

    Add basic auth

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 var express = require('express');
-
 var app = express();
 
+var auth = require('http-auth');
+var basic = auth.basic({
+    realm: 'Veuillez vous authentifier.',
+    file: __dirname + '/users.htpasswd',
+    algorithm: 'MD5'
+});
+
+app.use(auth.connect(basic));
 app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 5000);
