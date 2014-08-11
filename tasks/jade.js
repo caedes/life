875dd0c3ad14c75dafe00eb47db667549d7bac94
caedes <romain.laurent@wopata.com>
2014-08-11 16:54:26 +0200

    Sort events

--- tasks/jade.js
+++ tasks/jade.js
@@ -1,9 +1,12 @@
 var gulp  = require('gulp');
 var jade = require('gulp-jade');
+var _ = require('lodash');
 
 var config = require('../config.json');
 
 gulp.task('jade', function () {
+  config.sortedEvents = _.sortBy(config.events, 'date');
+
   gulp.src(config.paths.jade)
     .pipe(jade({
       locals: config
