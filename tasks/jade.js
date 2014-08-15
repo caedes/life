2c80ca55afa242a05434c24df436b14e1f12e813
caedes <romain.laurent@wopata.com>
2014-08-16 01:53:38 +0200

    Add moment.js french time

--- tasks/jade.js
+++ tasks/jade.js
@@ -1,10 +1,21 @@
 var gulp  = require('gulp');
 var jade = require('gulp-jade');
 var _ = require('lodash');
+var moment = require('moment');
 
 var config = require('../config.json');
 
 gulp.task('jade', function () {
+  moment.locale('fr');
+  moment.locale('fr', {
+    months: 'janvier février mars avril mai juin juillet août septembre octobre novembre décembre'.split(' ')
+  });
+  config.events.map(function(event){
+    event.displayDate = moment(event.date).format('D MMMM YYYY');
+
+    return event;
+  });
+
   config.sortedEvents = _.sortBy(config.events, 'date');
 
   gulp.src(config.paths.jade)
