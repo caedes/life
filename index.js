ea02f3e15cb1b7de7db502046d65928b3473be1b
caedes <romain.laurent@wopata.com>
2014-08-18 16:53:34 +0200

    Add some cache control

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ var basic = auth.basic({
     file: __dirname + '/users.htpasswd',
     algorithm: 'MD5'
 });
+var one_week = 7 * 24 * 3600;
 
 app.use(compress());
 app.use(auth.connect(basic));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public', { maxAge: one_week }));
 
 app.listen(process.env.PORT || 5000);
