f7c15f6130f54e36cf96e9e6855656671f3e4847
caedes <romain.laurent@wopata.com>
2014-08-18 17:12:17 +0200

    Remove HTTP Auth

--- index.js
+++ index.js
@@ -2,16 +2,9 @@ var express = require('express');
 var compress = require('compression');
 var app = express();
 
-var auth = require('http-auth');
-var basic = auth.basic({
-    realm: 'Veuillez vous authentifier.',
-    file: __dirname + '/users.htpasswd',
-    algorithm: 'MD5'
-});
 var one_week = 7 * 24 * 3600;
 
 app.use(compress());
-app.use(auth.connect(basic));
 app.use(express.static(__dirname + '/public', { maxAge: one_week }));
 
 app.listen(process.env.PORT || 5000);
