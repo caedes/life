b283ff527c565798c720b2ea41c4f8b0e396f45f
caedes <romain.laurent@wopata.com>
2014-08-21 11:29:56 +0200

    Improve CSS inclusion

--- tasks/stylus.js
+++ tasks/stylus.js
@@ -1,10 +1,12 @@
 var gulp  = require('gulp');
 var stylus = require('gulp-stylus');
+var concat = require('gulp-concat');
 
 var config = require('../config.json');
 
 gulp.task('stylus', function () {
   gulp.src(config.paths.stylus)
-    .pipe(stylus({compress: true}))
-    .pipe(gulp.dest(config.paths.build + '/css'));
+    .pipe(stylus())
+    .pipe(concat('app.css'))
+    .pipe(gulp.dest(config.paths.build));
 });
