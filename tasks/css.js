73d591740810046a855e9b7fb5d3d8a49bd820a1
caedes <romain.laurent@wopata.com>
2014-10-01 17:52:20 +0200

    Update css Gulp task for the build part

--- tasks/css.js
+++ tasks/css.js
@@ -6,6 +6,13 @@ var config = require('../config/gulp.json');
 
 gulp.task('css', function () {
   gulp.src(config.paths.stylus)
+    .pipe(stylus())
+    .pipe(concat('app.css'))
+    .pipe(gulp.dest(config.paths.build));
+});
+
+gulp.task('css:build', function () {
+  gulp.src(config.paths.stylus)
     .pipe(stylus({compress: true}))
     .pipe(concat('app.css'))
     .pipe(gulp.dest(config.paths.build));
