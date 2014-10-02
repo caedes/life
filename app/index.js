fb775369fe7b5b6ffaff7b43f2d1b4630cfe70ee
caedes <romain.laurent@wopata.com>
2014-10-02 18:10:27 +0200

    Move app.js into app/index.js

--- app/index.js
+++ app/index.js
@@ -5,6 +5,6 @@ var app = express();
 var one_week = 7 * 24 * 3600;
 
 app.use(compress());
-app.use(express.static(__dirname + '/public', { maxAge: one_week }));
+app.use(express.static('public', { maxAge: one_week }));
 
 app.listen(process.env.PORT || 5000);
